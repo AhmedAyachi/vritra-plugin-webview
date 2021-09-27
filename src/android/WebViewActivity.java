16d51fe65ce0d174d98f7b699b53cbca15cac62c
AhmedAyachi <aayachi032@gmail.com>
2021-09-27 19:17:28 +0100

    A

--- src/android/WebViewActivity.java
+++ src/android/WebViewActivity.java
@@ -3,6 +3,10 @@ package com.ahmedayachi.webview;
 import android.os.Bundle;
 import com.ahmedayachi.webview.WebView;
 import org.apache.cordova.*;
+import android.app.ActionBar;
+import android.view.Window;
+import android.view.View;
+import android.graphics.Color;
 
 
 
@@ -18,6 +22,10 @@ public class WebViewActivity extends CordovaActivity{
         String url="file:///android_asset/www/"+bundle.getString("url");
         message=bundle.getString("message");
         super.loadUrl(url);
+        final Window window=getWindow();
+        window.setStatusBarColor(Color.TRANSPARENT);
+        View decorview=getWindow().getDecorView();
+        decorview.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 
     public void onStart(){
