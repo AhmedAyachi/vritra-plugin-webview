6684dd978551c18bb703d031172c3501273a53b6
AhmedAyachi <aayachi032@gmail.com>
2021-10-13 06:48:25 +0100

    edit types

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,15 @@
 declare var WebView:WebView;
 
 interface WebView{
-    show:(options:{
+    show(options:{
         url:string,
         file:string,
         message:string,
-        onClose:(data:{message:string,store:object})=>void,
-    })=>void,
-    close:(message:string)=>void,
-    useMessage:(handler:(message:string)=>void)=>void,
-    setMessage:(message:string)=>void,
-    useStore:(handler:(store:object)=>void)=>void,
-    setStore:(key:string,value:any)=>void,
+        onClose(data:{message:string,store:object}):void,
+    }):void,
+    close(message:string):void,
+    useMessage(handler:(message:string)=>void):void,
+    setMessage(message:string):void,
+    useStore(handler:(store:object)=>void):void,
+    setStore(key:string,value:any):void,
 }
