5c63cf6623ea3fa39c885e4f691ba0e54b2049f2
AhmedAyachi <aayachi032@gmail.com>
2021-10-21 19:42:26 +0100

    A

--- src/android/ModalActivity.java
+++ src/android/ModalActivity.java
@@ -2,6 +2,9 @@ package com.ahmedayachi.webview;
 
 import com.ahmedayachi.webview.WebViewActivity;
 import android.os.Bundle;
+import android.view.Window;
+//import android.transition.Fade;
+import android.view.ViewGroup;
 
 
 public class ModalActivity extends WebViewActivity{
@@ -10,5 +13,10 @@ public class ModalActivity extends WebViewActivity{
     public void onCreate(Bundle savedInstanceState){
         this.isModel=true;
         super.onCreate(savedInstanceState);
+
+        final Window window=getWindow();
+        setTitle("");
+        window.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
+        //window.setExitTransition(new Fade());
     }
 }
